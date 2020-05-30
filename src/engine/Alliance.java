ff2adf648c9b5b10a313588827c945ff9b1d0c26
Mark Lucernas <marklcrns@gmail.com>
2020-05-30 16:17:16 -0700

    W4: Finished Alliance class Javadocs.

--- src/engine/Alliance.java
+++ src/engine/Alliance.java
@@ -1,6 +1,10 @@
 package engine;
 
 /**
+ * Enum class that holds the two opposing sides of the board game.
+ * This class is responsible of separating friendly and hostile pieces, their
+ * respective territories, and the alliance of the Players.
+ *
  * Author: Mark Lucernas
  * Date: 2020-05-18
  */
